72d66ccfe42dd714beb9126e5b3d9774d4ec26bf
LORD_CODE <83734728+LORD-ME-CODE@users.noreply.github.com>
2021-12-27 14:15:49 +0200

    Add files via upload

--- igruha/__init__.py
+++ igruha/__init__.py
@@ -41,14 +41,34 @@ class Igruha:
         responses.append(response)
         tree = BeautifulSoup(response.text, 'lxml')
         try:
-            lens = int(tree.find_all('a', {'onclick': "javascript:list_submit("}, partial=True)[-1].text)
+            try:
+                find = tree.find_all('a', {'onclick': lambda x: x.startswith("javascript:list_submit(") if x else False})
+                lens = int(find[-1].text)
+            except:
+                find = tree.find_all('input', {'id': 'dosearch'})
+                lens = int(find[-1].get('onclick').split('(')[1].split(')')[0])
+            self.data2['search_start'] = 2
             for i in range(2, lens + 1):
                 responses.append(self.session.post(url=self.url, data=self.data2, headers=self.headers))
                 self.data2['search_start'] += 1
-        except: pass
-        self.data2['search_start'] = 2
+        except:
+            pass
         self.result = AllResult([Result(response.text, self.session) for response in responses])
         return self.result
+
+    def main_games(self):
+        url = 'http://q.itorrents-igruha.org/'
+        h = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, utf-8',
+            'accept-language': 'ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
+            'cookie': 'cf_clearance=y9zIS75pSnpeSGbX0ZAOw5yVk7Ihi33kBqmb7rEmWmw-1640203407-0-150; PHPSESSID=2duinfc5nr5mve71sfdu1f23l0; __cf_bm=7aVPWkpFwFf2uHwaokFTcK3M_joPgBSLtL1E9K8fyDY-1640445672-0-ATLfN/EX+wRTM5pmqgfM+r5D8SvXKyDZv7bvtjbbCzTbGIob784Bv7vEZl+B9B/JGEL+SZjRNToPKClZ4PgMpSdkO6r+amzfaINCYAVg9KLUsAt3AWbm9wVmuYoeYya4DQ==',
+            'referer': 'https://www.google.com/',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62',
+        }
+        resp = self.session.get(url, headers=h).text
+        self.session.headers = self.headers
+        return Result(resp, self.session, 'main')
 
     def get_lastgame(self):
         return self.lastgame
@@ -59,6 +79,10 @@ class Igruha:
 
 class Game:
     def __init__(self, source, session):
+        self.torrent_names = None
+        self.torrents = None
+        self.ids = None
+        self.alls = None
         self.source = source
         self.session = session
         self.name = self.source.find('img', {'class': 'article-img'}).get('alt')
@@ -79,53 +103,52 @@ class Game:
         self.trailer = None
         self.screenshots = None
         self.id = None
-        self.torrent = 'https://q.itorrents-igruha.org/engine/download.php?id='+str(self.id)
+        self.torrent = 'https://q.itorrents-igruha.org/engine/download.php?id=' + str(self.id)
 
     def get_details(self):
         text = self.session.post(url=self.link).text
         tree = BeautifulSoup(text, "lxml").find('div', {'id': "dle-content"})
-        self.alls = tree.find('div', {'style': 'padding-left: 215px;'}).text.replace('<br>', "\n")
-        alls = '\n'.join(tree.find('div', {'style': "padding-left: 215px;"}).text.split('<br>'))
-        try: self.released = alls.split('Год выпуска: ')[1].split('Жанр')[0]
+        self.alls = str(tree.find('div', {'style': 'padding-left: 215px;'})).replace('<br>', "\n").\
+            replace('<br/>', '\n').replace('</br>', '\n').\
+            replace('<br />', '\n').replace('< br / >', '\n').\
+            replace('<br/ >', '\n').replace('< br/ >', '\n').\
+            replace('<div style="padding-left: 215px;">', '').\
+            replace('<b>', '').replace('</b>', '').\
+            replace('<div class="exampleone">', '').replace('</div>', '')
+        try:
+            self.requirements = self.alls.split('требования:')[1].split('\n\n')[0][1:]
         except:
-            try: self.released = alls.split('Дата выхода: ')[1].split('Жанр')[0]
-            except: pass
-        try: self.genre = alls.split('Жанр: ')[1].split('Разработчик')[0]
-        except: pass
-        try: self.developer = alls.split('Разработчик: ')[1].split('Издательство')[0]
-        except: pass
-        try: self.publisher = alls.split('Издательство: ')[1].split('Платформа')[0]
-        except: self.publisher = alls.split('Издатель: ')[1].split('Платформа')[0]
-        try: self.platform = alls.split('Платформа: ')[1].split('Тип')[0]
-        except: pass
-        try: self.edition_type = alls.split('Тип издания: ')[1].split('интерфейса')[0]
-        except: pass
-        try: self.lang_interface = alls.split('интерфейса: ')[1].split('озвучки: ')[0]
-        except: pass
-        try: self.lang_voice = alls.split('озвучки: ')[1].split('Таблетка')[0]
-        except: pass
-        try: self.tablet = alls.split('летка:')[1].split('Сист')[0]
-        except: print('WARN: Не удалось найти информацию про таблетку')
-        try: self.requirements = self.alls.split('Системные требования:')[1].split('\n\n')[0]
-        except: pass
-        try: self.description = tree.find('div', {'class': 'blockinfo'}).text.split('h2')[0].replace('<br>', '\n').\
-            replace('<ul>', '• ').replace('</ul>', '\n')
-        except: pass
+            pass
+        try:
+            self.description = tree.find('div', {'class': 'blockinfo'}).text.split('h2')[0].replace('<br>', '\n'). \
+                replace('<ul>', '• ').replace('</ul>', '\n').split('\n\n')[0]
+        except:
+            pass
 
-        try: self.trailer = 'https://youtu.be/'+tree.find('div', {'class': 'youtube'}).get('id')
-        except: pass
+        try:
+            self.trailer = 'https://youtu.be/' + tree.find('div', {'class': 'youtube'}).get('id')
+        except:
+            pass
 
         self.ids = [i.get('href').split('id=')[1] for i in tree.find_all('a', {'class': 'torrent'})]
+
         self.torrents = []
 
         x = tree.find_all('span', {'style': 'font-size:14pt;'})
-        self.torrent_names = [i.find('span', {'style': 'color: #89c80e;'}).text for i in x if i.find('span', {'style': 'color: #89c80e;'}) is not None]
+        self.torrent_names = [i.find('span', {'style': 'color: #89c80e;'}).text for i in x if
+                              i.find('span', {'style': 'color: #89c80e;'}) is not None]
 
-        for i in enumerate(tree.find_all('a', {'class': 'torrent'})):
-            self.torrents.append(Torrent(i[1], self.torrent_names[i[0]], self.session, self.name))
-
-        self.id = self.ids[0]
-        self.torrent = 'https://q.itorrents-igruha.org/engine/download.php?id=' + str(self.id)
+        try:
+            if len(self.ids) == 0:
+                for i in enumerate(tree.find_all('a', {'class': 'online'})):
+                    self.torrents.append(Torrent(i[1], self.torrent_names[i[0]], self.session, self.name))
+            else:
+                for i in enumerate(tree.find_all('a', {'class': 'torrent'})):
+                    self.torrents.append(Torrent(i[1], self.torrent_names[i[0]], self.session, self.name))
+                self.id = self.ids[0]
+                self.torrent = 'https://q.itorrents-igruha.org/engine/download.php?id=' + str(self.id)
+        except:
+            pass
         return self
 
     def to_dict(self):
@@ -144,7 +167,10 @@ class Torrent:
     def __init__(self, source, name, session, game_name):
         self.source = source
         self.session = session
-        self.link = 'https://q.itorrents-igruha.org/engine/download.php?id=' + self.source.get('href').split('id=')[1]
+        try:
+            self.link = 'https://q.itorrents-igruha.org/engine/download.php?id=' + self.source.get('href').split('id=')[1]
+        except:
+            self.link = self.source.get('href')
         self.name = name
         self.game_name = game_name
 
@@ -158,16 +184,23 @@ class Torrent:
 
 
 class Result:
-    def __init__(self, source: str, session: Sess):
+    def __init__(self, source: str = '', session: Sess = Sess(), typea: str = 'find'):
         self.source = source
         self.session = session
-        self.type = 'dle-content'
+
         self.tree = BeautifulSoup(self.source, "lxml")
-        self.game_list = self.tree.find('div', {'id': self.type})
-        if self.game_list is None:
-            raise Exception('Cloudflare не пропускает вас!')
-        self.games_source = self.game_list.find_all('div', {'class': 'article-film1'})
-        self.len = int(self.source.split('найдено ')[1].split(' ответов')[0])
+        self.type = typea
+        if self.type == 'main':
+            self.game_list = self.tree.find('div', {'class': 'global-container'})
+            if self.game_list is None:
+                raise Exception('Cloudflare не пропускает вас!')
+            self.games_source = self.game_list.find_all('div', {'class': 'article-film'})
+        else:
+            self.game_list = self.tree.find('div', {'id': 'dle-content'})
+            self.len = int(self.source.split('найдено ')[1].split(' ответов')[0])
+            if self.game_list is None:
+                raise Exception('Cloudflare не пропускает вас!')
+            self.games_source = self.game_list.find_all('div', {'class': 'article-film1'})
         self.games = [Game(i, self.session) for i in self.games_source]
 
 
@@ -179,10 +212,10 @@ class AllResult:
             self.games += i.games
 
 
-
 '''parser = Igruha()
+print(len(parser.search('call').games))
+print(parser.main_games().games[0].name)
 print(len(parser.search('call').games))
 game = parser.search('Far Cry').games[0]
 details = game.get_details()
 game.torrents[0].download()'''
-
